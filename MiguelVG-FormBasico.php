86c45db4ac3252f11eebabecfbe2cb23e2708237
Miguel-VG <a.18308052690041@cbtis.edu.mx>
2020-11-05 10:50:17 -0700

    Formularioo v1.2

--- MiguelVG-FormBasico.php
+++ MiguelVG-FormBasico.php
@@ -22,6 +22,87 @@
             <input type="text" name="domicilio" placeholder="Escribe tu domicilio">
             <input type="text" name="colonia" placeholder="Escribe tu colonia">
             <input type="text" name="codigopostal" placeholder="Escribe tu codigo postal">
+            <br>
+
+            <!-- Controles radio-->
+            <h1>Selecciona un mes - Radios </h1>
+            <input type="radio" name="radio1" value="opcion1"> Enero 
+            <br>
+            <input type="radio" name="radio1" value="opcion2"> Febrero
+            <br>
+            <input type="radio" name="radio1" value="opcion3"> 
+            Marzo
+            <br>
+            <input type="radio" name="radio1" value="opcion4"> 
+            Abril
+            <br>
+            <input type="radio" name="radio1" value="opcion5">
+             Mayo
+            <br>
+            <input type="radio" name="radio1" value="opcion6"> 
+            Junio
+            <br>
+            <input type="radio" name="radio1" value="opcion7">
+             Julio
+            <br>
+             <input type="radio" name="radio1" value="opcion8"> Agosto
+            <br>
+            <input type="radio" name="radio1" value="opcion9"> Septiembre
+            <br>
+            <input type="radio" name="radio1" value="opcion10"> Octubre
+            <br>
+            <input type="radio" name="radio1" value="opcion11"> Noviembre
+            <br>
+            <input type="radio" name="radio1" value="opcion12"> Diciembre
+            <br>
+         
+
+            <!-- Controles de tipo chekbox-->
+            <h1>Selecciona un mes - chekbox </h1>
+            <input type="checkbox" name="check1"> Enero
+            <br>
+            <input type="checkbox" name="check2"> Febrero
+            <br>
+            <input type="checkbox" name="check3"> Marzo
+            <br>
+            <input type="checkbox" name="check4"> Abril
+            <br>
+            <input type="checkbox" name="check5"> Mayo
+            <br>
+            <input type="checkbox" name="check6"> Junio
+            <br>
+            <input type="checkbox" name="check7"> Julio
+            <br>
+            <input type="checkbox" name="check8"> Agosto
+            <br>
+            <input type="checkbox" name="check9"> Septiembre
+            <br>
+            <input type="checkbox" name="check10"> Octubre
+            <br>
+            <input type="checkbox" name="check11"> Noviembre
+            <br>
+            <input type="checkbox" name="check12"> Diciembre
+            <br>
+
+            <!-- Controles Select-->
+            <h1>Selecciona un mes - select </h1>
+            <select name="seleccion">
+            <option value="ninguna">Seleccione una opcion</option>
+            <option value="seleccion1">Enero</option>
+            <option value="seleccion2">Febrero</option>
+            <option value="seleccion3">Marzo</option>
+            <option value="seleccion4">Abril</option>
+            <option value="seleccion5">Mayo</option>
+            <option value="seleccion6">Junio</option>
+            <option value="seleccion7">Julio</option>
+            <option value="seleccion8">Agosto</option>
+            <option value="seleccion9">Septimebre</option>
+            <option value="seleccion10">Octubre</option>
+            <option value="seleccion11">Noviembre</option>
+            <option value="seleccion12">Dieicembre</option>
+            </select>
+
+
             <input type="submit" name="boton" value="Enviar">
 
         </form>
--- obtener.php
+++ obtener.php
@@ -28,4 +28,131 @@ echo 'Tu grupo es  : '. $grupo. '<br>';
 echo 'Tu domicilio es : '. $domicilio. '<br>';
 echo 'Tu colonia es : '. $colonia. '<br>';
 echo 'Tu codigo postal es : '. $codigopostal. '<br>';
+
+echo '<br>';
+
+//Acceder a los radios
+
+
+if ($_REQUEST['radio1']== "opcion1") {
+   echo 'Seleccionaste el mes de enero'. '<br>'; 
+}
+else if ($_REQUEST['radio1']== "opcion2"){
+    echo'Seleccionaste el mes de febrero'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion3"){
+    echo'Seleccionaste el mes de marzo'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion4"){
+    echo'Seleccionaste el mes de abril'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion5"){
+    echo'Seleccionaste el mes de mayo'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion6"){
+    echo'Seleccionaste el mes de junio'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion7"){
+    echo'Seleccionaste el mes de julio'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion8"){
+    echo'Seleccionaste el mes de agosto'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion9"){
+    echo'Seleccionaste el mes de septiembre'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion10"){
+    echo'Seleccionaste el mes de octubre'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion11"){
+    echo'Seleccionaste el mes de noviembre'. '<br>';
+}
+else if ($_REQUEST['radio1']== "opcion12"){
+    echo'Seleccionaste el mes de diciembre'. '<br>';
+}
+
+//Acceder a los checkbox
+
+if (isset($_REQUEST['check1'])) {
+echo 'Seleccionaste enereo' . '<br>';    
+}
+if (isset($_REQUEST['check2'])) {
+echo 'Seleccionaste febrero' . '<br>';
+}
+if (isset($_REQUEST['check3'])) {
+echo 'Seleccionaste Marzo' . '<br>';
+}
+if (isset($_REQUEST['check4'])) {
+echo 'Seleccionaste Abril' . '<br>';
+}
+if (isset($_REQUEST['check5'])) {
+echo 'Seleccionaste Mayo' . '<br>';
+}
+if (isset($_REQUEST['check6'])) {
+echo 'Seleccionaste Junio' . '<br>';
+}
+if (isset($_REQUEST['check7'])) {
+    echo 'Seleccionaste Julio' . '<br>';
+    }
+ if (isset($_REQUEST['check8'])) {
+echo 'Seleccionaste agosto' . '<br>';
+}
+if (isset($_REQUEST['check9'])) {
+echo 'Seleccionaste septiembre' . '<br>';
+}
+if (isset($_REQUEST['check10'])) {
+echo 'Seleccionaste octubre' . '<br>';
+}
+if (isset($_REQUEST['check11'])) {
+    echo 'Seleccionaste noviembre' . '<br>';
+    }
+ if (isset($_REQUEST['check12'])) {
+        echo 'Seleccionaste diciembre' . '<br>';
+        }
+
+
+
+//accerder a los select
+
+if($_REQUEST['seleccion']=="ninguna"){
+  echo 'No seleccionaste ninguna opcion' . '<br>';  
+}
+else if ($_REQUEST['seleccion']== "seleccion1"){
+    echo 'Tu opcion es enero'. '<br>';
+}
+else if ($_REQUEST['seleccion']== "seleccion2") {
+ echo 'Tu opcion es febrero' . '<br>';   
+}
+else if ($_REQUEST['seleccion']== "seleccion3") {
+    echo 'Tu opcion es marzo' . '<br>';   
+   }
+   else if ($_REQUEST['seleccion']== "seleccion4") {
+ echo 'Tu opcion es abril' . '<br>' ;  
+}
+else if ($_REQUEST['seleccion']== "seleccion5") {
+ echo 'Tu opcion es mayo' . '<br>';   
+}
+else if ($_REQUEST['seleccion']== "seleccion6") {
+ echo 'Tu opcion es junio' . '<br>';   
+}
+else if ($_REQUEST['seleccion']== "seleccion7") {
+ echo 'Tu opcion es julio' . '<br>';   
+}
+else if ($_REQUEST['seleccion']== "seleccion8") {
+ echo 'Tu opcion es agosto' . '<br>' ;  
+}
+else if ($_REQUEST['seleccion']== "seleccion9") {
+    echo 'Tu opcion es septiembre' . '<br>' ;  
+   }
+   else if ($_REQUEST['seleccion']== "seleccion10") {
+    echo 'Tu opcion es octubre' . '<br>';   
+   }
+   else if ($_REQUEST['seleccion']== "seleccion11") {
+    echo 'Tu opcion es noviembre' . '<br>' ;  
+   }
+   else if ($_REQUEST['seleccion']== "seleccion12") {
+    echo 'Tu opcion es diciembre' . '<br>';   
+   } 
+
 ?>
+
